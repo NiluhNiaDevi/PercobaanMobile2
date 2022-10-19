ccbffa64932e9c6ccd399ec9ab5723c51f378299
NiluhNiaDevi <109826288+NiluhNiaDevi@users.noreply.github.com>
2022-10-19 18:47:36 +0800

    Add files via upload

---
+++ NiluhNiaDevi.java
@@ -0,0 +1,18 @@
+package com.NiluhNiaDeviF55121058.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class NiluhNiaDevi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niluh_nia_devi);
+    }
+}
